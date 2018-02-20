9e7b305d1d94d8a9b1340b712c4481ecb1357fd8
Ian Harwell <sirharwell@gmail.com>
2018-02-20 14:06:10 -0700

    update 1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var methodOverride = require('method-overrride')
+var methodOverride = require('method-override')
 
 var index = require('./routes/index');
 var moives = require('./routes/movies')
--- routes/movies.js
+++ routes/movies.js
@@ -2,6 +2,19 @@ var express = require('express');
 var router = express.Router();
 var Movie = require('../models').Movie;
 
+router.get('/:id/edit', function(req, res) {
+  Movie.findById(req.params.id)
+  .then( function(movie) {
+    return res.render('edit', {movie: movie})
+  })
+})
+
+router.delete('/:id', functions(req, res) {
+  Movie.findById(req.params.id)
+  .then( function(movie) { movie.destroy() })
+  .then( function() {return res.redirect('/movies')})
+})
+
 router.get('/', function(req, res) {
   Movie.all()
     .then( function(movies) {
